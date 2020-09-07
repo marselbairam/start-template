8645bf9064d311726f516c831374ae07adc76149
Marsel Bairamov <marselbairam@gmail.com>
2020-09-07 08:59:28 +0300

    upd config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   extends: [
     'stylelint-config-airbnb',
     'stylelint-prettier/recommended',
+    "stylelint-config-rational-order"
   ],
   plugins: [
     'stylelint-order',
-    'stylelint-scss',
+    "stylelint-config-rational-order/plugin",
+    'stylelint-scss'
   ],
   ignoreFiles: [
     './markup/static/scss/entry/*.scss',
